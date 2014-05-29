fab965eb7e87d5f3635f068b87d5f15fbc98102b
christianbergau <cbergau86@gmail.com>
2014-05-29 15:42:30 +0200

    fix some small issues

--- src/Bws/DoctrineBundle/DependencyInjection/Configuration.php
+++ src/Bws/DoctrineBundle/DependencyInjection/Configuration.php
@@ -18,12 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('bws_doctrine');
-
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
-
+        $treeBuilder->root('bws_doctrine');
         return $treeBuilder;
     }
 }
