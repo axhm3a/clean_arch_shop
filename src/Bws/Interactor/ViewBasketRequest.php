c7fafaefb2c5b5eedb292b8ea13e4676133c0935
christianbergau <cbergau86@gmail.com>
2014-05-29 19:19:38 +0200

    PSR-2 Fix

--- src/Bws/Interactor/ViewBasketRequest.php
+++ src/Bws/Interactor/ViewBasketRequest.php
@@ -4,23 +4,25 @@ namespace Bws\Interactor;
 
 class ViewBasketRequest 
 {
+    /**
+     * @var int
+     */
     private $basketId;
 
+    /**
+     * @param int $basketId
+     */
     public function __construct($basketId)
     {
         $this->basketId = $basketId;
     }
 
     /**
-     * @return integer
+     * @return int
      */
     public function getBasketId()
     {
         return $this->basketId;
     }
-
-
-
-
 }
  
