a0524fc275513457113f44836ba661bc645e4a5d
christianbergau <cbergau86@gmail.com>
2014-06-14 10:36:18 +0200

    Rename controller

--- src/BwsShop/WebBundle/Controller/ArticleController.php
+++ src/BwsShop/WebBundle/Controller/ArticleController.php
@@ -7,12 +7,12 @@ use FOS\RestBundle\Controller\Annotations\View;
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Request;
 
-class ArticlesController extends FOSRestController
+class ArticleController extends FOSRestController
 {
     public function indexAction()
     {
         $articles = $this->get('interactor.present_articles')->execute()->getArticles();
-        return $this->render('BwsShopWebBundle:ListArticles:index.html.twig', array('articles' => $articles));
+        return $this->render('BwsShopWebBundle:Article:index.html.twig', array('articles' => $articles));
     }
 
     /**
@@ -29,7 +29,7 @@ class ArticlesController extends FOSRestController
 
         $view = $this
             ->view($articles, 200)
-            ->setTemplate('BwsShopWebBundle:ListArticles:index.html.twig')
+            ->setTemplate('BwsShopWebBundle:Article:index.html.twig')
             ->setTemplateVar('articles');
 
         return $this->handleView($view);
@@ -40,7 +40,7 @@ class ArticlesController extends FOSRestController
         $article = $this->get('interactor.present_article')->execute($request->get('id'))->getArticle();
         $view    = $this
             ->view($article, 200)
-            ->setTemplate('BwsShopWebBundle:ListArticles:view.html.twig')
+            ->setTemplate('BwsShopWebBundle:Article:view.html.twig')
             ->setTemplateVar('article');
 
         return $this->handleView($view);
