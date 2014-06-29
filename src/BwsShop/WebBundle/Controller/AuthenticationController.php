942f787d22dbca5ec85a39be80d1e8bb0f1cfdda
christianbergau <cbergau86@gmail.com>
2014-06-29 13:48:50 +0200

    Show basket empty message if try to visit checkout

--- src/BwsShop/WebBundle/Controller/AuthenticationController.php
+++ src/BwsShop/WebBundle/Controller/AuthenticationController.php
@@ -3,12 +3,18 @@
 namespace BwsShop\WebBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class AuthenticationController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('BwsShopWebBundle:Authentication:index.html.twig');
+        $session = new Session();
+        if ($session->get('total') == 0) {
+            return $this->render('BwsShopWebBundle:Authentication:basket.empty.html.twig');
+        } else {
+            return $this->render('BwsShopWebBundle:Authentication:index.html.twig');
+        }
     }
 }
  
